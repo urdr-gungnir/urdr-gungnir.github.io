2640f73583c3fdbc956c5336a2375819cdeaca30
gungnir <502591592@qq.com>
2024-03-01 11:07:20 +0800

    Site updated: 2024-03-01 11:07:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/25/享受生活/减脂理论/","2024/02/24/安全相关/安全岗面经（自刷用）/"];function toRandomPost(){
+var posts=["2024/02/25/享受生活/减脂理论/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
