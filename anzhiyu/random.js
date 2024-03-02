8012d9f29b03df56921a8313672a2ebbc65bc3dd
gungnir <502591592@qq.com>
2024-03-02 21:40:43 +0800

    Site updated: 2024-03-02 21:40:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/25/享受生活/减脂理论/"];function toRandomPost(){
+var posts=["2024/02/25/享受生活/减脂理论/","2024/03/02/享受生活/我的未来几个月的减脂计划/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
